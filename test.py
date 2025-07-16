4146196ab674a9f9c19a69d8a984a26f01a43002
fahime9407 <fahime.emlaei@gmail.com>
2025-07-16 22:07:35 +0330

    deleting test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import math
-
-v = float(input("Enter wind speed in kilometers/hour : "))
-t = float(input("Enter air temperature in degrees celsius : "))
-
-wci = 13.12 + 0.6215 * t - 11.37 * math.pow(v, 0.16) + 0.3965 * t * math.pow(v, 0.16)
-
-print(f"I dont know wci!!!")
