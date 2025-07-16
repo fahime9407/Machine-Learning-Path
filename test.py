71498026d41fffad2ba60313d2b2e8972aeaec1a
fahime9407 <fahime.emlaei@gmail.com>
2025-07-16 21:28:57 +0330

    update test

--- test.py
+++ test.py
@@ -6,3 +6,5 @@ t = float(input("Enter air temperature in degrees celsius : "))
 wci = 13.12 + 0.6215 * t - 11.37 * math.pow(v, 0.16) + 0.3965 * t * math.pow(v, 0.16)
 
 print(f"I dont know wci!!!")
+
+print(12 + "fahime")
